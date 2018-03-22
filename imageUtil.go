119ec9a96b9ebdd9e4941d1456211d1eee2e86cb
Andy Carrell <carrellandrew@gmail.com>
2018-03-22 21:50:32 +1300

    Tidy image utils

--- imageUtil.go
+++ imageUtil.go
@@ -36,19 +36,35 @@ func getStartingPoint(c string) image.Point {
 	return image.Pt(x*100, y*100)
 }
 
+func getRect(x, y int) image.Rectangle {
+	return image.Rect(x*100, 0, y*100, 100)
+}
+
+func drawFor(new draw.Image, src image.Image) func(r image.Rectangle, sp image.Point) {
+	return func(r image.Rectangle, sp image.Point) {
+		draw.Draw(new, r, src, sp, draw.Over)
+	}
+}
+
+func shouldPrintComma(count, pos int) bool {
+	return count > 3 && (count-pos)%3 == 0
+}
+
 func getImage(n int) draw.Image {
 	cs := strconv.Itoa(n)
 
 	srcImg := readImage()
 	newImg := image.NewRGBA(image.Rect(0, 0, (len(cs)+(len(cs)-1)/3)*100, 100))
+	d := drawFor(newImg, srcImg)
 
 	commaOffset := 0
 	for pos, c := range cs {
-		if (len(cs)-pos)%3 == 0 {
-			draw.Draw(newImg, image.Rect((pos+commaOffset)*100, 0, (pos+1+commaOffset)*100, 100), srcImg, getStartingPoint(","), draw.Over)
+		if shouldPrintComma(len(cs), pos) {
+			d(getRect((pos+commaOffset), (pos+1+commaOffset)), getStartingPoint(","))
 			commaOffset++
 		}
-		draw.Draw(newImg, image.Rect((pos+commaOffset)*100, 0, (pos+1+commaOffset)*100, 100), srcImg, getStartingPoint(string(c)), draw.Over)
+		d(getRect((pos+commaOffset), (pos+1+commaOffset)), getStartingPoint(string(c)))
 	}
+
 	return newImg
 }
